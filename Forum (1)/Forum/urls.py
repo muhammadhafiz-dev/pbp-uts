d13cdd319883ef3d02aeb157f6a9a65781b92334
Muhammad Hafiz Hanan <muhammad.hafiz43@ui.ac.id>
2025-10-23 18:54:34 +0700

    perbaikan

--- Forum (1)/Forum/urls.py
+++ Forum (1)/Forum/urls.py
@@ -1,32 +1,13 @@
-"""
-URL configuration for sportnet project.
+from django.urls import path
+from . import views  # Impor views dari app forum
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include, re_path
+app_name = 'forum' # Namespace agar tidak bentrok
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Homepage.urls')),
-    path('authenticate/', include('Authenticate.urls')),
-    # path('booked-events/', include('BookedEvent.urls')),
-    # path('bookmark/', include('Bookmark.urls')),
-    path('forum/', include('forum.urls')),
-    path('event/', include("Event.urls")),
-    # path('forum/', include("Forum.urls")),
-    # path('notification/', include('Notification.urls')),
-    # path('profile/', include('Profile.urls')),
-    path('review/', include('reviews.urls')),
+
+    path(
+        'event/<int:event_id>/', 
+        views.forum_page_view, 
+        name='forum_page' 
+    ),
 ]
